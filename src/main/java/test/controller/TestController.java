6b7124762b6aad24a7c7058442f88461d9c2ee2c
Ken-Yuan, Kan <410122037@gms.ndhu.edu.tw>
2023-07-28 15:08:05 +0800

    testcommit

--- src/main/java/test/controller/TestController.java
+++ src/main/java/test/controller/TestController.java
@@ -16,6 +16,7 @@ public class TestController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		String xxx = req.getParameter("xxx");
+		System.out.println("123");
 		req.setAttribute("nickname", xxx);
 		req.getRequestDispatcher("/index.jsp").forward(req, res);
 	}
